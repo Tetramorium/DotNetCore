b16bdd807d5a5d1d6ad2cc38c989bae2c520adf8
Jelmer Mulder <9375891+Tetramorium@users.noreply.github.com>
2018-11-13 14:43:05 +0100

    Loggigng

--- DotNetCoreWeb/DotNetCoreWeb_001/Pages/Create.cshtml.cs
+++ DotNetCoreWeb/DotNetCoreWeb_001/Pages/Create.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace DotNetCoreWeb_001.Pages
 {
@@ -11,15 +12,18 @@ namespace DotNetCoreWeb_001.Pages
     {
         private readonly AppDbContext db;
 
+        private ILogger<CreateModel> Log;
+
         [TempData]
         public string Message { get; set; }
 
         [BindProperty]
         public Customer Customer { get; set; }
 
-        public CreateModel(AppDbContext _db)
+        public CreateModel(AppDbContext _db, ILogger<CreateModel> _log)
         {
             this.db = _db;
+            this.Log = _log;
         }
 
         public async Task<IActionResult> OnPostAsync()
@@ -31,7 +35,11 @@ namespace DotNetCoreWeb_001.Pages
 
             db.Customers.Add(Customer);
             await db.SaveChangesAsync();
-            Message = $"Customer {Customer.Name} added!";
+            var msg = Message = $"Customer {Customer.Name} added!";
+           // Message = msg;
+
+            Log.LogCritical(msg);
+
             return RedirectToPage("/Index");
         }
     }
--- DotNetCoreWeb/DotNetCoreWeb_001/Startup.cs
+++ DotNetCoreWeb/DotNetCoreWeb_001/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DotNetCoreWeb_001
 {
@@ -36,12 +37,17 @@ namespace DotNetCoreWeb_001
                     options.UseInMemoryDatabase("Test")
                 );
 
+            services.AddLogging();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
